e40ef289773facdb35f76e6d3f6d62d6b64db717
Saleban Abdillahi <01saleban@gmail.com>
2021-12-02 05:57:17 +0000

    add new function to client.js

--- client.js
+++ client.js
@@ -10,6 +10,7 @@ const connect = function () {
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
     conn.write('Name: Sal')
+    conn.write("Move: up");
     
   });     
 
